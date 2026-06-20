d291f3deb5aca6203f205b7f0c1c8a932b491a5b
anjum <44854471+mohammedwaseemanjum@users.noreply.github.com>
2026-06-21 03:12:42 +0800

    changes on connection turso

--- db/index.ts
+++ db/index.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const client = createClient({
-  url: process.env.TURSO_CONNECTION_URL!,
+  url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
